af799bf7d9978e4654f94e90a6f4e80364970e0a
yajie <yajie.sun24@gmail.com>
2016-01-26 18:07:27 +0100

    Change format

--- DecisionTree.py
+++ DecisionTree.py
@@ -3,11 +3,12 @@ import math
 
 class DecisionTree(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def makeTree(self, X, Y):
-		"""Construct decision tree
+    def makeTree(self, X, Y):
+        """
+        Construct decision tree
 
         Parameters
         ----------
@@ -18,44 +19,45 @@ class DecisionTree(object):
         Returns
         -------
         tree: a tree node stored in dictionary.
-        	  key: the key of the dictionary is name of the classifier.
-        	  value: a list. the element of the list are lists of length 3.
-        	  	  List[0]: value of the classifier.
-        	  	  List[1]: decision if the tree has no subtrees.
-        	  	  List[2]: subtrees.
+              key: the key of the dictionary is name of the classifier.
+              value: a list. the element of the list are lists of length 3.
+                  List[0]: value of the classifier.
+                  List[1]: decision if the tree has no subtrees.
+                  List[2]: subtrees.
         """
-		attributes = list(X.columns.values)
-		label = list(Y.columns.values)[0]
-		data = pd.concat([X, Y], axis=1)
+        attributes = list(X.columns.values)
+        label = list(Y.columns.values)[0]
+        data = pd.concat([X, Y], axis=1)
 
-		attribute, decisions = self.getBestAttribute(data, label, attributes)
-		# No information gain. Done classifying.
-		if not attribute:
-			return 
+        attribute, decisions = self.getBestAttribute(data, label, attributes)
+        # No information gain. Done classifying.
+        if not attribute:
+            return 
 
-		attributes.remove(attribute)
-		dataGroups = data.groupby(attribute)
-		tree, branches = {}, []
+        attributes.remove(attribute)
+        dataGroups = data.groupby(attribute)
+        tree, branches = {}, []
 
-		for key in dataGroups.groups.keys():
-			branch = [key, decisions[key], None]
-			branches.append(branch)
-		tree[attribute] = branches
+        for key in dataGroups.groups.keys():
+            branch = [key, decisions[key], None]
+            branches.append(branch)
+        tree[attribute] = branches
 
-		for group in dataGroups.groups:
-			subData = dataGroups.get_group(group)
-			X = subData.drop([label, attribute], axis=1)
-			Y = pd.DataFrame(subData[label])
+        for group in dataGroups.groups:
+            subData = dataGroups.get_group(group)
+            X = subData.drop([label, attribute], axis=1)
+            Y = pd.DataFrame(subData[label])
 
-			# Find which branch the current subtree belongs to
-			for i in xrange(len(tree[attribute])):
-				if tree[attribute][i][0] == group:
-					tree[attribute][i][2] = self.makeTree(X, Y)
-					break
-		return tree
+            # Find which branch the current subtree belongs to
+            for i in xrange(len(tree[attribute])):
+                if tree[attribute][i][0] == group:
+                    tree[attribute][i][2] = self.makeTree(X, Y)
+                    break
+        return tree
 
-	def computeEntropy(self, data, label):
-		"""Compute entropy of a dataset using the classifier label.
+    def computeEntropy(self, data, label):
+        """
+        Compute entropy of a dataset using the classifier label.
 
         Parameters
         ----------
@@ -67,19 +69,20 @@ class DecisionTree(object):
         -------
         entropy : entropy value        
         """
-		dataGroups = data.groupby(label)
-		total = float(len(data))
-		entropy = 0
+        dataGroups = data.groupby(label)
+        total = float(len(data))
+        entropy = 0
 
-		for group in dataGroups.groups:
-			freq = len(dataGroups.get_group(group))
-			if freq == 0:
-				continue
-			entropy += -freq / total * math.log(freq / total, 2)
-		return entropy
+        for group in dataGroups.groups:
+            freq = len(dataGroups.get_group(group))
+            if freq == 0:
+                continue
+            entropy += -freq / total * math.log(freq / total, 2)
+        return entropy
 
-	def getBestAttribute(self, data, label, attributes):
-		"""Select best attribute from attributes that maximize entropy gain
+    def getBestAttribute(self, data, label, attributes):
+        """
+        Select best attribute from attributes that maximize entropy gain
 
         Parameters
         ----------
@@ -94,28 +97,28 @@ class DecisionTree(object):
         bestAttr: attribute name that maximize entropy gain or None
         decision: The most frequent label value in current dataset.
         """
-		entropy = self.computeEntropy(data, label)
-		# Intialize the default values. The default decision is the most frequent label value
-		maxGain, bestAttr, decisions = 0, None, [data[label].value_counts().keys()[0]]
+        entropy = self.computeEntropy(data, label)
+        # Intialize the default values. The default decision is the most frequent label value
+        maxGain, bestAttr, decisions = 0, None, [data[label].value_counts().keys()[0]]
 
-		# Compute entropy gain for each attribute
-		for attribute in attributes:
-			tempdecisions = {}
-			dataGroups = data.groupby(attribute)
-			total = float(len(data))
-			subentropy = 0
+        # Compute entropy gain for each attribute
+        for attribute in attributes:
+            tempdecisions = {}
+            dataGroups = data.groupby(attribute)
+            total = float(len(data))
+            subentropy = 0
 
-			for group in dataGroups.groups:
-				subData = dataGroups.get_group(group)
-				freq = len(dataGroups.get_group(group))
-				subentropy += freq / total * self.computeEntropy(subData, label)
-				decision = subData[label].value_counts().keys()[0]
-				tempdecisions[group] = decision
+            for group in dataGroups.groups:
+                subData = dataGroups.get_group(group)
+                freq = len(dataGroups.get_group(group))
+                subentropy += freq / total * self.computeEntropy(subData, label)
+                decision = subData[label].value_counts().keys()[0]
+                tempdecisions[group] = decision
 
-			gain = entropy - subentropy
-			if gain > maxGain:
-				bestAttr = attribute
-				maxGain = gain
-				decisions = tempdecisions
+            gain = entropy - subentropy
+            if gain > maxGain:
+                bestAttr = attribute
+                maxGain = gain
+                decisions = tempdecisions
 
-		return bestAttr, decisions
+        return bestAttr, decisions
